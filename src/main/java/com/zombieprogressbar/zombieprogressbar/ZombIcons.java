e7edb19f6ff7dd6b65cf049cf8487eb39771bfa8
shushiro <miriam.uni@outlook.de>
2022-06-10 20:54:00 +0200

    initial commit

---
+++ src/main/java/com/zombieprogressbar/zombieprogressbar/ZombIcons.java
@@ -0,0 +1,10 @@
+package com.zombieprogressbar.zombieprogressbar;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface ZombIcons {
+    Icon ZOMBIE_ICON = IconLoader.getIcon("/rsz_zombie.png");
+    Icon RZOMBIE_ICON = IconLoader.getIcon("/rsz_rzombie.png");
+}
---
+++ src/main/java/com/zombieprogressbar/zombieprogressbar/ZombieApplicationComponent.java
@@ -0,0 +1,17 @@
+package com.zombieprogressbar.zombieprogressbar;
+
+import com.intellij.ide.ui.LafManager;
+
+import javax.swing.*;
+
+public class ZombieApplicationComponent {
+    public ZombieApplicationComponent() {
+        LafManager.getInstance().addLafManagerListener(__ -> updateProgressBarUi());
+        updateProgressBarUi();
+    }
+
+    private void updateProgressBarUi() {
+        UIManager.put("ProgressBarUI", ZombieProgressBarUi.class.getName());
+        UIManager.getDefaults().put(ZombieProgressBarUi.class.getName(), ZombieProgressBarUi.class);
+    }
+}
