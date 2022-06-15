b17900cf597f01ea8fff2648b8119231ce5e2548
shushiro <miriam.uni@outlook.de>
2022-06-15 15:47:15 +0200

    replace IconLoader.getIcon() deprecation

--- src/main/java/ZombIcons.java
+++ src/main/java/ZombIcons.java
@@ -1,8 +1,8 @@
-import com.intellij.openapi.util.IconLoader;
-
+import com.intellij.util.ui.JBImageIcon;
 import javax.swing.*;
 
 public interface ZombIcons {
-    Icon ZOMBIE_ICON = IconLoader.getIcon("/rsz_zombie.png");
-    Icon RZOMBIE_ICON = IconLoader.getIcon("/rsz_rzombie.png");
+
+    Icon ZOMBIE_ICON = new JBImageIcon(new ImageIcon("/rsz_zombie.png").getImage());
+    Icon RZOMBIE_ICON = new JBImageIcon(new ImageIcon("/rsz_rzombie.png").getImage());
 }
